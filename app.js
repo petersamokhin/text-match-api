887b48bcdbcaa99aa5bc3a82805061fe39896f2b
petersamokhin <contact@petersamokhin.com>
2018-05-05 19:44:46 +0300

    Changed path for routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/text-matcher', mainRouter);
+app.use('/', mainRouter);
 app.use(errorHandler);
 
 const port = 3001;
